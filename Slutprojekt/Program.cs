4c547e7b6e82f93b3b5f3f51bb3b4ed93e97c911
Theo Lee <theo.leehao@elev.ga.ntig.se>
2024-03-06 12:03:35 +0100

    name loop

--- Slutprojekt/Program.cs
+++ Slutprojekt/Program.cs
@@ -1,4 +1,19 @@
 ﻿
 System.Console.WriteLine("Welcome to the colosseum");
-System.Console.WriteLine();
+System.Console.WriteLine("You have been summoned to participate in the yearly fighting tournament");
+System.Console.WriteLine("State your name peaseant");
+String player = Console.ReadLine();
+while(true)
+    if (player != "")
+    {
+        break;
+    }
+    else if (player == "")
+    {
+        System.Console.WriteLine("YOUR KING DEMANDS YOU TO STATE YOUR NAME!");
+        player = Console.ReadLine();
+    }
+System.Console.WriteLine("Well then peaseant prepare for battle ");
+
+
 Console.ReadLine();
