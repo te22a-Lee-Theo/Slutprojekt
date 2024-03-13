d0d98868edc0e5d43d075da2444e254f79128687
Theo Lee <theo.leehao@elev.ga.ntig.se>
2024-03-13 11:13:03 +0100

    klass player skapad

--- Slutprojekt/Program.cs
+++ Slutprojekt/Program.cs
@@ -1,22 +1,25 @@
-﻿
+﻿Player player = new Player();
+
 System.Console.WriteLine("Welcome to the colosseum");
 System.Console.WriteLine("You have been summoned to participate in the yearly fighting tournament");
 System.Console.WriteLine("State your name peaseant");
-String player = Console.ReadLine();
+String playername = Console.ReadLine();
 while(true)
-    if (player != "")
+{
+    if (playername != "")
     {
         break;
     }
-    else if (player == "")
+    else if (playername == "")
     {
         System.Console.WriteLine("YOUR KING DEMANDS YOU TO STATE YOUR NAME!");
-        player = Console.ReadLine();
+        playername = Console.ReadLine();
     }
+}
 System.Console.WriteLine("Well then peaseant prepare for battle.");
 System.Console.WriteLine("You have been placed against the weakest in this colosseum and you only way to freedom is to fight for it.");
 
 
-while()
+while(player.hp <= 0 ||)
 
 Console.ReadLine();
--- Slutprojekt/fighters.cs
+++ Slutprojekt/fighters.cs
@@ -1,4 +1,6 @@
-public class player
+public class Player
 {
-    
+    public int hp = 50;
+
+    public int BaseDmg = 10;
 }
